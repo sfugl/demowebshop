551e1dd9d9a428e29f57f6176ca5b0ec901a0a29
sebastianfugl <sebastian.fugl@me.com>
2019-07-13 14:57:32 +0200

    first

---
+++ README.md
@@ -0,0 +1,4 @@
+# Demo WebShop
+Simple web shop REST api using Spring Boot
+
+
---
+++ src/main/java/com/tigerbow/demowebshop/DemoWebShopApplication.java
@@ -0,0 +1,13 @@
+package com.tigerbow.demowebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebShopApplication.class, args);
+	}
+
+}
